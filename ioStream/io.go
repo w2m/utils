a89e01ddd6669cddb841d5f968d896100e6a67ce
bigwish <359851485@qq.com>
2018-06-26 12:38:13 +0800

    修改可连续添加write

--- ioStream/io.go
+++ ioStream/io.go
@@ -8,8 +8,9 @@ type WriterStream struct {
 	writers []io.Writer
 }
 
-func (w *WriterStream) AddWriter(writer io.Writer) {
+func (w *WriterStream) AddWriter(writer io.Writer) *WriterStream {
 	w.writers = append(w.writers, writer)
+	return w
 }
 
 func (w *WriterStream) Write(p []byte) (n int, err error) {
